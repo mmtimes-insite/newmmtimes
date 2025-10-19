2e591d4579af680c894c34fbc2d75eaaa9030946
mmtimes-insite <mmtimes2023@gmail.com>
2025-10-19 16:18:16 +0530

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,36 @@
+<!-- ✅ Structured Data (SEO Schema) -->
+<script type="application/ld+json">
+{
+  "@context": "https://schema.org",
+  "@type": "Organization",
+  "name": "MEENAKSHI MUKUNDHA TIMES",
+  "url": "https://www.mmtimes.in",
+  "logo": "https://www.mmtimes.in/assets/images/logo.png",
+  "sameAs": [
+    "https://www.linkedin.com/company/mmtimes",
+    "https://www.instagram.com/mmtimes_insta/",
+    "https://www.youtube.com/@mmtimes_meenakshimukundhatimes",
+    "https://www.facebook.com/profile.php?id=61581371498673",
+    "https://x.com/mmtimes_1"
+  ],
+  "contactPoint": [{
+    "@type": "ContactPoint",
+    "telephone": "+91-79892613425",
+    "contactType": "customer service",
+    "email": "info@mmtimes.in"
+  }],
+  "address": {
+    "@type": "PostalAddress",
+    "streetAddress": "Chittavaram",
+    "addressLocality": "Chittavaram",
+    "addressRegion": "Andhra Pradesh",
+    "postalCode": "521182",
+    "addressCountry": "IN"
+  }
+}
+</script>
+
+
+
+
+
